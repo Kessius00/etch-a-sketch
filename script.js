ced6d929ff7b26415d86b7db59a3d65fd91ec53e
kessius00 <kes.olling00@gmail.com>
2022-08-21 20:37:00 +0200

    remove function randColor for an even more random color generator with math.random()

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ grid(); changeBackgroundColor();
 
 //EVENT LISTENERS
 slider.addEventListener('input', refreshGrid);
-// color.addEventListener('input', changeColor());
 randBtn.addEventListener('click', btnStatusChange);
+// color.addEventListener('input', changeColor);
 
 
 
@@ -69,7 +69,7 @@ function changeBackgroundColor(){
         box.addEventListener('mouseover', () => {
 
         if (randBtnOn === true){
-            box.style.backgroundColor = randomColor();    
+            box.style.backgroundColor = `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`;    
         } else box.style.backgroundColor = pointerColor;
         });
 
@@ -79,24 +79,6 @@ function changeBackgroundColor(){
     });
 };
 
-function randomColor(listOfColors=['black']){
-    //function which returns a random color from a list of colors
-    if (listOfColors.length === 1){
-        listOfColors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
-
-    };
-    const index = Math.floor(Math.random()*listOfColors.length);
-    return listOfColors[index]
-};
 
 function btnStatusChange(){
     if (randBtnOn===true){
@@ -104,8 +86,8 @@ function btnStatusChange(){
     } else randBtnOn = true;
 };
 
-// function changeColor(){
-//     const colorValue = color.value;
+function changeColor(){
+    const colorValue = color.value;
     
-//     return 
-// };
+    return true
+};
