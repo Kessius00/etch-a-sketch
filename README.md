c374de7e52e9d88637187c7cbf765c3e85ccfabe
kessius00 <kes.olling00@gmail.com>
2022-08-23 17:33:46 +0200

    Show GridSize, show grid, add Eraser and Incrementally Darken button

--- README.md
+++ README.md
@@ -1,23 +1,7 @@
-# etch-a-sketch 
+# To Do
+- RandomizeColor
+- RandomizeShade
+- Make CSS Grid
+- Make range input functional
+- Make Buttonpress functional (change borderCOlor and textcolor)
 
-The goal was to create a screen for a person to make pixelart on, where the pixels could have various sizes.
-
-# Lessons Learned 💭
-
-# Optimization ✅
-- Make range slider beautiful
-- randomize shade grey
-- incrementally darken 10%
-- pick a color
-- 
-
-add more css for more beauty
-
-
-
-
-# Tech Used 💻
-HTML5 CSS JavaScript
-
-# Made by ⚒️
-Kes Olling
--- script.js
+++ script.js
@@ -1,4 +1,48 @@
 //CONSTANTS
 const boxContainer = document.querySelector('.boxContainer');
+const slider = document.querySelector('.slider');
+const color = document.querySelector('#color');
+const gridSize = document.querySelector('.gridSize');
+
+
+
+
+createDivs();
+createGrid(slider.value); displayGridSize();
+
+//EVENTLISTENERS
+slider.addEventListener('input', refreshGrid);
+
+
+
 
 //FUNCTIONS
+function refreshGrid(){
+    removeCurrentGrid();
+    createDivs(slider.value);
+    createGrid(slider.value);
+    displayGridSize();
+}
+
+function displayGridSize(){
+    gridSize.textContent = `${slider.value}x${slider.value}`;
+}
+
+function removeCurrentGrid(){
+    while (boxContainer.firstChild) {
+        boxContainer.removeChild(boxContainer.lastChild);
+    };
+}
+
+function createDivs( amount = 16 ){
+    // Make grid with optional parameter (sqrt of amount of boxes)
+    for (let i=0; i<(amount**2); i++){
+        let box = document.createElement('div');
+        box.classList.toggle('box');
+        boxContainer.appendChild(box);
+    };
+};
+
+function createGrid(amount){
+    boxContainer.style.cssText=`grid-template-columns: repeat(${amount}, auto);`;
+}
